1557cec2871f095daee61d4285f01920b273f8d5
wangjunjie <wangjunjie@360.net>
2018-11-23 21:38:45 +0800

    commit

--- 工具/util/Date.js
+++ 工具/util/Date.js
@@ -19,3 +19,27 @@ Date.prototype.format = function(format) {
 	}
 	return format;
 }
+
+
+function formDateTime (time, format) {
+	time = new Date(time)
+	var o = {
+		"M+": time.getMonth() + 1, // month
+		"d+": time.getDate(), // day
+		"h+": time.getHours(), // hour
+		"m+": time.getMinutes(), // minute
+		"s+": time.getSeconds() // second
+	};
+    
+	if (/(y+)/.test(format)) {
+		format = format.replace(RegExp.$1, (time.getFullYear() + "").substr(4 - RegExp.$1.length));
+	}
+	
+	for (var k in o) {
+		if (new RegExp("(" + k + ")").test(format)) {
+			format = format.replace(RegExp.$1,
+		    RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+		}
+	}
+	return format;
+}
